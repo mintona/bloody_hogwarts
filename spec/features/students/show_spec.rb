043f54ea914a380570d74fed231378f99bf7e20f
Alison Vermeil <alison.minton@gmail.com>
2019-12-06 09:58:58 -0700

    Add robustness to tests and complete user story 2

--- spec/features/students/show_spec.rb
+++ spec/features/students/show_spec.rb
@@ -11,19 +11,28 @@ RSpec.describe "As a visitor" do
                                   age: 14,
                                   house: "Gryffindor")
 
-      course_1 = Course.create!(name: "Defense against the Dark Arts")
+      course_1 = Course.create!(name: "Defense Against the Dark Arts")
       course_2 = Course.create!(name: "Herbology")
       course_3 = Course.create!(name: "Potions")
 
       student_1.courses << course_1
       student_1.courses << course_2
       student_1.courses << course_3
-      
-      visit "students/#{student_1.id}"
+
+      student_2.courses << course_1
+      student_2.courses << course_3
+
+      visit "/students/#{student_1.id}"
 
       expect(page).to have_content(course_1.name)
       expect(page).to have_content(course_2.name)
       expect(page).to have_content(course_3.name)
+
+      visit "/students/#{student_2.id}"
+
+      expect(page).to have_content(course_1.name)
+      expect(page).to have_content(course_3.name)
+      expect(page).to_not have_content(course_2.name)
     end
   end
 end
