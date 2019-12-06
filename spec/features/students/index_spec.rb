572006d090477b5b48f7a045bec0ce8f5fbc3935
Alison Vermeil <alison.minton@gmail.com>
2019-12-06 09:42:27 -0700

    User story 1 complete

---
+++ spec/features/students/index_spec.rb
@@ -0,0 +1,27 @@
+require 'rails_helper'
+
+RSpec.describe "As a visitor" do
+  describe "when I visit the students index page" do
+    describe "I see a list of students" do
+      it "name, age and house" do
+        student_1 = Student.create!(name: "Casseopia Black",
+                                    age: 14,
+                                    house: "Slytherin")
+
+        student_2 = Student.create!(name: "Harry Potter",
+                                    age: 14,
+                                    house: "Gryffindor")
+
+        visit '/students'
+save_and_open_page
+        expect(page).to have_content(student_1.name)
+        expect(page).to have_content(student_1.age)
+        expect(page).to have_content(student_1.house)
+
+        expect(page).to have_content(student_2.name)
+        expect(page).to have_content(student_2.age)
+        expect(page).to have_content(student_2.house)
+      end
+    end
+  end
+end
