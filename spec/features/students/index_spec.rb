3a8a72ef44ff5ef61f7334fce634688116e6ce27
Alison Vermeil <alison.minton@gmail.com>
2019-12-06 10:36:28 -0700

    Add model and feature test for sorting students in alphabetical order

--- spec/features/students/index_spec.rb
+++ spec/features/students/index_spec.rb
@@ -22,6 +22,29 @@ RSpec.describe "As a visitor" do
         expect(page).to have_content(student_2.age)
         expect(page).to have_content(student_2.house)
       end
+
+      it "in alphabetical order" do
+        student_1 = Student.create!(name: "Casseopia Black",
+                                    age: 14,
+                                    house: "Slytherin")
+
+        student_2 = Student.create!(name: "Harry Potter",
+                                    age: 14,
+                                    house: "Gryffindor")
+
+        student_3 = Student.create!(name: "Hermonie Granger",
+                                    age: 12,
+                                    house: "Gryffindor")
+
+        student_4 = Student.create!(name: "Draco Malfroy",
+                                    age: 14,
+                                    house: "Slytherin")
+        visit "/students"
+        
+        expect(page.body.index("Casseopia Black")).to be < page.body.index("Draco Malfroy")
+        expect(page.body.index("Draco Malfroy")).to be < page.body.index("Harry Potter")
+        expect(page.body.index("Harry Potter")).to be < page.body.index("Hermonie Granger")
+      end
     end
 
     it "has the average age of all students" do
--- spec/models/student_spec.rb
+++ spec/models/student_spec.rb
@@ -33,6 +33,26 @@ RSpec.describe Student, type: :model do
 
       expect(Student.average_age).to eq(13.5)
     end
+
+    it "can sort students in alphabetical order" do
+      student_1 = Student.create!(name: "Casseopia Black",
+                                  age: 14,
+                                  house: "Slytherin")
+
+      student_2 = Student.create!(name: "Harry Potter",
+                                  age: 14,
+                                  house: "Gryffindor")
+
+      student_3 = Student.create!(name: "Hermonie Granger",
+                                  age: 12,
+                                  house: "Gryffindor")
+
+      student_4 = Student.create!(name: "Draco Malfroy",
+                                  age: 14,
+                                  house: "Slytherin")
+
+      expect(Student.alphabetical).to eq([student_1, student_4, student_2, student_3])
+    end
   end
 
 end
