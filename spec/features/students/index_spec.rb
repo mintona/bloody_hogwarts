3ace689e8f498d44e05e819c1c96d4c32882dd8d
Alison Vermeil <alison.minton@gmail.com>
2019-12-06 10:22:30 -0700

    Add eature and model tests for average age of students

--- spec/features/students/index_spec.rb
+++ spec/features/students/index_spec.rb
@@ -23,5 +23,27 @@ RSpec.describe "As a visitor" do
         expect(page).to have_content(student_2.house)
       end
     end
+
+    it "has the average age of all students" do
+      student_1 = Student.create!(name: "Casseopia Black",
+                                  age: 14,
+                                  house: "Slytherin")
+
+      student_2 = Student.create!(name: "Harry Potter",
+                                  age: 14,
+                                  house: "Gryffindor")
+
+      student_3 = Student.create!(name: "Hermonie Granger",
+                                  age: 12,
+                                  house: "Gryffindor")
+
+      student_4 = Student.create!(name: "Draco Malfroy",
+                                  age: 14,
+                                  house: "Slytherin")
+
+      visit "/students"
+save_and_open_page
+      expect(page).to have_content("Average Age: 13.5")
+    end
   end
 end
--- spec/models/student_spec.rb
+++ spec/models/student_spec.rb
@@ -13,4 +13,26 @@ RSpec.describe Student, type: :model do
     it {should have_many(:courses).through(:student_courses)}
   end
 
+  describe 'methods' do
+    it "can calculate average age of students" do
+      student_1 = Student.create!(name: "Casseopia Black",
+                                  age: 14,
+                                  house: "Slytherin")
+
+      student_2 = Student.create!(name: "Harry Potter",
+                                  age: 14,
+                                  house: "Gryffindor")
+
+      student_3 = Student.create!(name: "Hermonie Granger",
+                                  age: 12,
+                                  house: "Gryffindor")
+
+      student_4 = Student.create!(name: "Draco Malfroy",
+                                  age: 14,
+                                  house: "Slytherin")
+
+      expect(Student.average_age).to eq(13.5)
+    end
+  end
+
 end
